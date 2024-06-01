5d7df4e89f711d70bd7d4da81b4ee1df42e5e603
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-01 07:42:26 -0700

    add a traffic light programe

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+
+var trafficLightColor = prompt("Keep The Right Direction")
+
+if(trafficLightColor === "red"){
+    alert("Stop")
+}
+else if(trafficLightColor === "yellow"){
+    alert("Get Ready")
+}
+else if(trafficLightColor === "green"){
+    alert("Go")
+} 
+else{
+    alert("Invilid Color")
+}
