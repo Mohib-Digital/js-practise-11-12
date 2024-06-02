220f6b8c3e4e9cc74b87a1678896ac5f9f23ecdc
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-02 05:23:48 -0700

    add few file and codes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,116 @@
 
-var trafficLightColor = prompt("Keep The Right Direction")
+// Chapter.11  (Comparison Operators)
 
-if(trafficLightColor === "red"){
-    alert("Stop")
+// equal ===, ==
+// ==
+// console.log(1==1)
+// console.log(1=="1")
+// console.log(3 == 5)
+
+// ===
+// console.log(1===1)
+// console.log(1==="1")
+// console.log(3 == 5)
+
+//  not (!)
+// console.log(!true)
+// console.log(!false)
+
+// not equal (!=, !==)
+// (!=)
+// console.log(1 != 1) //false
+// console.log(1 != "1") //false
+// console.log(1 != 2) //ture
+
+// (!==)
+// console.log(1 !== 1) //false
+// console.log(1 !== "1") //true
+// console.log(1 !== 2) //ture
+
+
+// >
+// console.log(1 > "0") //true
+
+// <
+
+// >=
+
+// <=
+
+
+
+// =====================================================================================================
+// =====================================================================================================
+
+// Chapter.No 12 (if else and else if statement)
+
+// Program:  ODD/EVEN
+var yourName = prompt("Please enter your name:")
+var anyNum = +prompt("Please enter any number:")
+
+var remainder = anyNum % 2
+
+if (remainder === 0) {
+    alert(yourName + " Bhai ap nai even number enter kia")
 }
-else if(trafficLightColor === "yellow"){
-    alert("Get Ready")
+else {
+    alert (yourName + " Bhai ap nai odd number enter kia")
 }
-else if(trafficLightColor === "green"){
-    alert("Go")
-} 
-else{
-    alert("Invilid Color")
-}
+// =====================================================================================================
+
+// Enter correct username
+// var myName = prompt("Enter my name to get the access!")
+
+// if (myName === "Mohib Ullah") {
+//     alert("Correct! You allow to login.")
+// }
+// if (myName !== "Mohib Ullah") {
+//     alert("Sorry! Try again leter!")
+// }
+
+// var myName = prompt("Enter my name to get the access!")
+
+// if (myName === "Mohib Ullah") {
+//     alert("Correct! You allow to login.")
+// }
+// else {
+//     alert("Sorry! Try again leter!")
+// }
+// ===============================================================================================
+
+// var trafficLightColor = prompt("Keep The Right Direction")
+
+// if(trafficLightColor === "red"){
+//     alert("Stop")
+// }
+// else if(trafficLightColor === "yellow"){
+//     alert("Get Ready")
+// }
+// else if(trafficLightColor === "green"){
+//     alert("Go")
+// } 
+// else{
+//     alert("Invilid Color")
+// }
+// =============================================================================================
+
+// var shopingAmount = +prompt("Give the discount on there needs!")
+
+// if(shopingAmount <= 4999){
+//     alert("Give the discount of 10%")
+// }
+// else if(shopingAmount <= 9999){
+//     alert("Give the discount of 25%")
+// }
+// else if(shopingAmount >= 10000){
+//     alert("Give the discount of 50%")
+// }
+// ==============================================================================================
+
+// var BadalHaiBahi = true;
+// console.log(!BadalHaiBahi)
+
+
+// =================== The End ==================
+
+console.log(2 * "3")
